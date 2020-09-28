37301de17df7072d3ae33c0052855b6ab2e14aac
yuya-estuary <kawagiti826@gmail.com>
2020-09-28 18:04:42 +0900

    Add validation

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -69,6 +69,6 @@ class MessagesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def message_params
-      params.require(:message).permit(:title, :message)
+      params.require(:message).permit(:title, :message, :name)
     end
 end

5399ebf4a80e4b77492cc2bfad63a9c589c24b22
yuya-estuary <kawagiti826@gmail.com>
2020-09-28 21:40:31 +0900

    Add name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200922134608) do
+ActiveRecord::Schema.define(version: 20200928123729) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20200922134608) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "name"
+    t.integer  "year"
   end
 
 end

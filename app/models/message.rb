775bd0d3db13ea3612a358758a9e7ddc284821ee
yuya-estuary <kawagiti826@gmail.com>
2020-09-29 13:04:42 +0900

    Add validation

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,5 @@ class Message < ApplicationRecord
     validates :title , length: { maximum: 32 } , presence: true
     validates :message , length: { maximum: 256 } , presence: true
     validates :name , length: { maximum: 32 }
+    validates :year , length: { maximum: 4 }
 end
